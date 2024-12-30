d10b6fa0842413a696f637b951800037700e53c4
Tom Bartoccioni <tbartocc@student.42nice.fr>
2024-12-30 15:39:13 +0100

    echo

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,33 +6,48 @@
 /*   By: tbartocc <tbartocc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/01 18:11:58 by tbartocc          #+#    #+#             */
-/*   Updated: 2024/11/04 14:28:26 by tbartocc         ###   ########.fr       */
+/*   Updated: 2024/12/30 15:32:08 by tbartocc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../src.h"
 
+int	pass_args(t_parser *parser, int *i)
+{
+	int	j;
+
+	while (parser->cmd[*i])
+	{
+		if (parser->cmd[*i][0] != '-' || parser->cmd[*i][1] != 'n')
+			break ;
+		j = 2;
+		while (parser->cmd[*i][j] == 'n')
+			j++;
+		if (parser->cmd[*i][j] != '\0' && parser->cmd[*i][j] != 'n')
+			break ;
+		(*i)++;
+	}
+	if ((*i) == 1)
+		return (1);
+	return (0);
+}
+
 int	my_echo(t_env *env, t_parser *parser)
 {
 	int	i;
-	int	no_newline;
+	int	n;
 
 	(void)env;
 	i = 1;
-	no_newline = 0;
-	if (parser->cmd[1] && ft_strcmp(parser->cmd[1], "-n") == 0)
-	{
-		no_newline = 1;
-		i++;
-	}
+	n = pass_args(parser, &i);
 	while (parser->cmd[i])
 	{
-		printf("%s", parser->cmd[i]);
+		ft_printf("%s", parser->cmd[i]);
 		if (parser->cmd[i + 1])
-			printf(" ");
+			ft_printf(" ");
 		i++;
 	}
-	if (!no_newline)
+	if (n)
 		printf("\n");
 	return (0);
 }
